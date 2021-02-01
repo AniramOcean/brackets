c63d6f72ed90db54188a6f012358561bac568765
Marina Grishina <52827985+AniramOcean@users.noreply.github.com>
2021-02-01 22:07:18 +0300

    feat: implement brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 === 0) {
+        for (let i = str.length; i >= 0; i--) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                if (str[i] === bracketsConfig[j][0]) {
+                    str = str.replace(bracketsConfig[j].join(''), '');
+                }
+            }
+        }
+    }
+    return str === '';
 }
